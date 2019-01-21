264960bc6068dbf5fad9a5de3a3fdb9c3d6a39e6
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-21 15:45:48 +0100

    Added enumerate method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import request from 'request';
 
 const isBridgeConnected = async () => {
-    request.get('http://localhost:21325/status/', {
+    request.get('http://127.0.0.1:21325/status/', {
     }, (error, response) => {
         if (error || response.statusCode !== 200) {
-            throw Error(`Bridge is not connected', ${error}`);
+            throw Error(`Cannot connect to bridge', ${error}`);
         } else {
             console.log('Bridge is connected');
             return true;
+        }
+    });
+};
+
+const enumerate = async () => {
+    request.post('http://127.0.0.1:21325/enumerate', {
+        headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:21325',
+        },
+    }, (error, response) => {
+        if (error) {
+            console.log('error');
+            throw Error(`Listen error, is bridge connected?', ${error}`);
+        } else {
+            console.log('response', response.toJSON());
+            return response.body;
         }
     });
 };
@@ -28,5 +44,9 @@ const isBridgeConnected = async () => {
 // });
 
 (async () => {
+    console.log('aaa');
     await isBridgeConnected();
+    const devices = await enumerate();
+
+    console.log('device', devices);
 })();
