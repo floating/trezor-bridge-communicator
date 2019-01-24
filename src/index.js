abfedf6451ea669c76645b50848277c8d97b21aa
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-24 16:23:09 +0100

    Added debug messages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import request from 'request';
 import protobufjs from 'protobufjs';
 import loadDevice from '../loadDevice.json';
 
+const isDebug = true;
+
 const TYPES = {
     LOAD_DEVICE: 13,
 };
@@ -16,6 +18,9 @@ const isBridgeConnected = async () => new Promise(((resolve, reject) => {
         if (error || response.statusCode !== 200) {
             reject(error);
         } else {
+            if (isDebug) {
+                console.log('Bridge is connected');
+            }
             resolve(true);
         }
     });
@@ -23,6 +28,9 @@ const isBridgeConnected = async () => new Promise(((resolve, reject) => {
 ));
 
 const getDevices = async () => new Promise(((resolve, reject) => {
+    if (isDebug) {
+        console.log('get devices start');
+    }
     request.post('http://127.0.0.1:21325/enumerate', {
         headers: {
             Origin: 'https://wallet.trezor.io',
@@ -31,12 +39,18 @@ const getDevices = async () => new Promise(((resolve, reject) => {
         if (error) {
             reject(error);
         } else {
+            if (isDebug) {
+                console.log('get devices success');
+            }
             resolve(res.toJSON().body);
         }
     });
 }));
 
 const acquire = async (path, previousSession) => new Promise(((resolve, reject) => {
+    if (isDebug) {
+        console.log('start acquiring session on path', path);
+    }
     request.post(`http://127.0.0.1:21325/acquire/${path}/${previousSession}`, {
         headers: {
             Origin: 'https://wallet.trezor.io',
@@ -50,8 +64,10 @@ const acquire = async (path, previousSession) => new Promise(((resolve, reject) 
     });
 }));
 
-
 const acquireDebug = async (path, previousSession) => new Promise(((resolve, reject) => {
+    if (isDebug) {
+        console.log('start acquiring debug session on path', path);
+    }
     request.post(`http://127.0.0.1:21325/debug/acquire/${path}/${previousSession}`, {
         headers: {
             Origin: 'https://wallet.trezor.io',
@@ -60,12 +76,16 @@ const acquireDebug = async (path, previousSession) => new Promise(((resolve, rej
         if (error) {
             reject(error);
         } else {
+            console.log('successfully acquired debug session with path', path);
             resolve(JSON.parse(res.toJSON().body));
         }
     });
 }));
 
 const release = async session => new Promise(((resolve, reject) => {
+    if (isDebug) {
+        console.log('start release session', session);
+    }
     request.post(`http://127.0.0.1:21325/release/${session}`, {
         headers: {
             Origin: 'https://wallet.trezor.io',
@@ -74,6 +94,27 @@ const release = async session => new Promise(((resolve, reject) => {
         if (error) {
             reject(error);
         } else {
+            console.log('successfully released session', session);
+            resolve(res.toJSON().body);
+        }
+    });
+}));
+
+const releaseDebug = async session => new Promise(((resolve, reject) => {
+    if (isDebug) {
+        console.log('start release debug session', session);
+    }
+    request.post(`http://127.0.0.1:21325/debug/release/${session}`, {
+        headers: {
+            Origin: 'https://wallet.trezor.io',
+        },
+    }, (error, res) => {
+        if (error) {
+            reject(error);
+        } else {
+            if (isDebug) {
+                console.log('successfully released debug session', session);
+            }
             resolve(res.toJSON().body);
         }
     });
@@ -88,7 +129,9 @@ const compose = (buf, type) => {
 };
 
 const callHex = async (session, hex) => new Promise(((resolve, reject) => {
-    console.log('calling hex', hex, 'with sesion', session);
+    if (isDebug) {
+        console.log('calling hex', hex, 'with sesion', session);
+    }
     request.post(`http://127.0.0.1:21325/call/${session}`, {
         body: hex,
         headers: {
@@ -104,7 +147,9 @@ const callHex = async (session, hex) => new Promise(((resolve, reject) => {
 }));
 
 const postHexDebug = async (session, hex) => new Promise(((resolve, reject) => {
-    console.log('calling post', hex, 'with sesion', session);
+    if (isDebug) {
+        console.log('calling post', hex, 'with sesion', session);
+    }
     request.post(`http://127.0.0.1:21325/debug/post/${session}`, {
         body: hex,
         headers: {
@@ -163,4 +208,8 @@ const initDevice = async session => new Promise(((resolve, reject) => {
 
     // response confirm on device
     await postHexDebug(acquiredDebugDevice.session, ACTIONS.CONFIRM);
+
+    // release sessions
+    await release(acquiredDevice.session);
+    await releaseDebug(acquiredDebugDevice.session);
 })();
